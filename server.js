c9c9a80f903c5723df5fa9ad3a5aff4dbc19a5ae
Amanda Surber <surberaleigh@gmail.com>
2020-04-04 05:35:39 -0400

    setting up heroku and adding link to server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 app.use(express.static("public"))
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+var MONGODB_URI = process.env.MONGODB_URI || "http://afternoon-cliffs-68969.herokuapp.com/"
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
