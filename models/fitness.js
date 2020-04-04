e57a78abae838def84dc1225f2d4b1b760dfa622
Amanda Surber <surberaleigh@gmail.com>
2020-04-04 04:22:43 -0400

    updating index and fitness.js

--- models/fitness.js
+++ models/fitness.js
@@ -1,20 +1,55 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema(
-    {
-        day: {
-            type: Date,
-            default: Date.now
+const fitnessSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    excercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Exercise Type"
         },
-        excercises: [
-            {
-                type:{
-                    type:String,
-                    trim:true,
-                    required:"Enter an excercise type"
-                }
-            }
-        ]
+        name: {
+            type: String,
+            trim: true,
+            required: "Exercise Name"
+        },
+        duration: {
+            type: Number,
+            required: "Exercise Duration In Minutes"
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
     }
-)
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+fitnessSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Fitness = mongoose.model("Fitness", fitnessSchema);
+
+module.exports = Fitness;
